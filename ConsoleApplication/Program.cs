bc8cc9ef3011dfd8e563ac3a18af7f3810acfd35
Vira Kyzyma <vera.kyzyma8@gmail.com>
2024-07-01 08:14:04 +0300

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string childProcessPath = "notepad.exe";
+            Process childProcess = new Process();
+
+            childProcess.StartInfo.FileName = childProcessPath;
+            childProcess.StartInfo.UseShellExecute = false;
+            childProcess.StartInfo.RedirectStandardOutput = true;
+            childProcess.StartInfo.RedirectStandardError = true;
+            childProcess.StartInfo.CreateNoWindow = true;
+            childProcess.EnableRaisingEvents = true;
+
+            childProcess.Exited += (sender, EventArgs) =>
+            {
+                int exiteCode = childProcess.ExitCode;
+
+                Console.WriteLine($"Дочерний процесс завершился кодом {exiteCode}");
+                childProcess.Dispose();
+            };
+            childProcess.Start();
+            childProcess.WaitForExit();
+        }
+    }
+}
